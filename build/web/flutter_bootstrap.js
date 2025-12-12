5de02d71269accf7db06c0f78cd5b5c2d8dc09bb
nathan <nathansilvadeandrade22@gmail.com>
2025-12-11 21:01:27 -0300

    fixing

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "994005606"
+    serviceWorkerVersion: "3311249533"
   }
 });
--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "cors-fix.js": "eff9ac8c6cb314bb3ccaf3a8c416cc50",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "d3cdc95a90ffeac4ecde86c3bdf1482c",
+"flutter_bootstrap.js": "efccd02159a7d1801e7d1acdfb1160cd",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
